c7d7fa7a424a5defd3ff2c1218a4af37288bfa35
Black candy <122829729+blackcandy-54@users.noreply.github.com>
2023-04-29 20:23:05 +0530

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,18 +1,20 @@
 import socket
+from pyrogram import Client
 
 def get_ip_address():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
     return ip_address
 
-# Telegram Bot code to send the IP address to a chat
-import pyrogram
+# Replace api_id, api_hash, bot_token and chat_id with your own values
+api_id = "24254966"
+api_hash = "ef0f6eb46def4eef272f26e1907fa736"
+bot_token = "6044349131:AAEe2vJF5K5ehytiMP9i4aPVPQi_yX0Sg_8"
+chat_id = "1345158291"
 
-bot_token = ''
-chat_id = ''
+app = Client("my_account", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-bot = telegram.Bot(token=bot_token)
-ip_address = get_ip_address()
-
-message = f"My server's IP address is: {ip_address}"
-bot.send_message(chat_id=chat_id, text=message)
+with app:
+    ip_address = get_ip_address()
+    message = f"My server's IP address is: {ip_address}"
+    app.send_message(chat_id=chat_id, text=message)
